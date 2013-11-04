c87c73d59651fa6b7144029a447409ca321cdd75
Caleb Everett <everett.caleb@gmail.com>
2013-11-04 14:33:12 -0500

    Base repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WikiGraphMysql::Application.config.secret_key_base = 'cd697c4daa7e668eeb64539ce9bbbbc8ef7657bf2bbe1bd1f865803e1a3c6c7f2e6978cb0636dc76bd8a30ebb34f6e90ae6dddba3a9cdb38f55024751035b1f9'
