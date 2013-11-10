d7636161f529798a829ccb1fdd7d5ca4c53a0e16
Caleb Everett <everett.caleb@gmail.com>
2013-11-09 22:47:21 -0500

    Update db/schema (should this be commited?)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131104193704) do
+ActiveRecord::Schema.define(version: 20131110015807) do
 
   create_table "links", force: true do |t|
     t.integer "to_id"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20131104193704) do
   create_table "pages", force: true do |t|
     t.string  "title"
     t.integer "page_ident"
-    t.integer "namespace"
     t.integer "wiki_id"
   end
 
